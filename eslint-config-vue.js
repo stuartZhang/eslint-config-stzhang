7f2b6d06fae89dc4c7fe4649756801e7ba9678cb
stuart_zhang <stuartpage@qq.com>
2018-02-23 19:05:30 +0800

    添加Vue规则配置集。

---
+++ eslint-config-vue.js
@@ -0,0 +1,11 @@
+'use strict';
+// Variables
+module.exports = {
+  'extends': [
+    'plugin:vue/strongly-recommended'
+  ],
+  'plugin': ['vue'],
+  'rules': {
+    'vue/max-attributes-per-line': ['error', {'singleline': 10, 'multiline': 5}]
+  }
+};
