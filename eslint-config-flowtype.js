1864c6cebc35fbed55659310cd1f045056d2d330
stuart_zhang <stuartpage@qq.com>
2018-06-20 10:08:25 +0800

    添加 facebook 的 flowtype 类型描述。

---
+++ eslint-config-flowtype.js
@@ -0,0 +1,18 @@
+'use strict';
+// Facebook Flow
+module.exports = {
+  'extends': [
+    'plugin:flowtype/recommended'
+  ],
+  'plugins': [
+    'flowtype'
+  ],
+  'settings': {
+    'flowtype': { // flow.js 规则仅应用于 // @flow 文件。
+      'onlyFilesWithFlowAnnotation': true
+    }
+  },
+  'rules': {
+    'flowtype/union-intersection-spacing': ['error', 'never']
+  }
+};
