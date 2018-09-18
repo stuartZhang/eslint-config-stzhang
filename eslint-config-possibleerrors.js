d1692d44bacf22d21040b9adad2eff2114ef37b0
stuart_zhang <stuartpage@qq.com>
2018-09-18 14:40:18 +0800

    允许 console.time 与 console.timeEnd

--- eslint-config-possibleerrors.js
+++ eslint-config-possibleerrors.js
@@ -2,7 +2,7 @@
 module.exports = {
   'rules': {
     'no-console': [process.env.NODE_ENV === 'production' ? 'error' : 'warn', {
-      'allow': ['assert', 'error']
+      'allow': ['assert', 'error', 'time', 'timeEnd']
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
