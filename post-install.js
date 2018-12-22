fb9eb4d27f562a956673f593abb127fb1e445bb7
stuart_zhang <stuartpage@qq.com>
2018-12-22 14:21:57 +0800

    被安装之后，自动将 自身 依赖的 eslint-plugin-*** 插件 上移至 紧上一层 node_modules 目录里。如果 紧上一层 不是 node_modules 目录 就什么也不做。

---
+++ post-install.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const path = require('path');
+const co = require('co');
+const pkg = require('./package.json');
+
+const parentDir = path.join(__dirname, '..');
+const parentDirName = path.basename(parentDir);
+
+if (parentDirName === 'node_modules') {
+  Object.keys(pkg.dependencies).filter(name => /^eslint-plugin-/.test(name)).forEach(co.wrap(function *(name, i){
+    const src = path.join(__dirname, 'node_modules', name);
+    if (yield fs.pathExists(src)) {
+      const dest = path.join(parentDir, name);
+      if (yield fs.pathExists(dest)) {
+        yield fs.remove(dest);
+      }
+      yield fs.move(src, dest);
+      console.log(`${i + 1}. 安装 ESLint 插件依赖 ${name}`);
+    }
+  }));
+}
