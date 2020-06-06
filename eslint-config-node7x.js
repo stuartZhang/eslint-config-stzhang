51988649b43a2841ba2df15165f5f17091f17917
stuart_zhang <stuartpage@qq.com>
2020-06-06 15:21:37 +0800

    修复缺陷：规则合并出现了错误。

--- eslint-config-node7x.js
+++ eslint-config-node7x.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const node8 = require('./eslint-config-node8x');
+module.exports = require('./eslint-config-node8x');
 
-module.exports = Object.assign({}, node8, {
-  rules: {
+Object.assign(module.exports.rules, {
     'require-unicode-regexp': 'off',
     'prefer-destructuring': 'off',
     'prefer-object-spread': 'off',
     'no-param-reassign': 'off',
     'prefer-reflect': 'off',
     'array-bracket-newline': 'off'
-  }
 });
