d271e7e9f4252ae3a5e668212093b01276aad9f5
stuart_zhang <stuartpage@qq.com>
2020-06-23 10:47:16 +0800

    改善 tsx 配置。

--- eslint-config-vue-tsx.js
+++ eslint-config-vue-tsx.js
@@ -2,8 +2,14 @@ const _ = require('underscore');
 
 module.exports = require('./eslint-config-vue-ts');
 
+_.defaults(module.exports.parserOptions, {
+  ecmaFeatures: {}
+});
 _.extendOwn(module.exports.parserOptions, {
   createDefaultProgram: true,
   useJSXTextNode: true,
   callback: _.noop
+});
+_.extendOwn(module.exports.parserOptions.ecmaFeatures, {
+  jsx: true
 });
